d8ab887283937fbd29a57399699fd60a0d45d3e3
Barreirosc <jpaullobr@gmail.com>
2022-08-22 12:39:53 -0300

    Leitura/Escrita dados

---
+++ LeituraEscritaDeDadosEmArquivo/src/exericio01/Exercicio01.java
@@ -0,0 +1,45 @@
+package exericio01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+
+		ArrayList<String> linhas = new ArrayList<String>();
+		
+		String tarefa;
+		int i = 0;
+		while (true) { 
+			System.out.print("Tarefa " + (i + 1) + ": ");
+			tarefa = scanner.nextLine();
+			
+			if ("x".equals(tarefa)) {
+				break; // Parar o laço.
+			}
+
+			linhas.add(tarefa);
+			i++;
+		}
+
+		escreverNoArquivo("C:\\Users\\HASEST\\Documents\\tarefas-diaria", linhas);
+
+		scanner.close();
+		
+		System.out.println("Fim...");
+	}
+	
+	static void escreverNoArquivo(String arquivo, ArrayList<String> linhas) throws IOException {
+		Path path = Paths.get(arquivo);
+		Files.write(path, linhas);
+		
+		
+	}
+	
+}
---
+++ LeituraEscritaDeDadosEmArquivo/src/leituraarquivos/LeituraArquivos.java
@@ -0,0 +1,22 @@
+package leituraarquivos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LeituraArquivos {
+	public static void main(String[] args) throws IOException {
+		Path arquivo = Paths.get("C:\\Users\\HASEST\\Documents\\arquivo.txt");
+		
+		List<String> linhas =  Files.readAllLines(arquivo);
+		
+		for(int i = 0; i < linhas.size(); i++) {
+			String nomes = linhas.get(i);
+
+			System.out.println("Jogador " + (i + 1) + ": " + nomes);
+		}
+		
+	}
+}
